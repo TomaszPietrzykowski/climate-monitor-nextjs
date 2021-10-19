6f1034ff1a0ee05216effd88056322f4d651bd8c
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2021-10-19 22:35:58 +0200

    robots.txt and sitemap.xml creation automated with postbuild script

--- scripts/sitemap.js
+++ scripts/sitemap.js
@@ -7,23 +7,40 @@ const robotsTxt = `User-agent: *
 Sitemap: https://climatemonitor.info/sitemap_local.xml
 Disallow:`
 
-fs.writeFileSync("public/robots.txt", robotsTxt)
+fs.writeFileSync("out/robots.txt", robotsTxt)
 console.log("robots.txt saved!")
 
 // SITEMAP.XML
 const pathsObj = getPathsObject()
 const today = formatDate(new Date())
+
+// datasets paths
+const datasets = [
+  "co2",
+  "ch4",
+  "sf6",
+  "n2o",
+  "temperatures",
+  "sealevels",
+  "glaciers",
+]
+
 const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${Object.keys(pathsObj)
-    .filter((path) => path !== "/_document" && path !== "/_app")
+    .filter(
+      (path) =>
+        path !== "/_document" && path !== "/_app" && path !== "/data/[dataset]"
+    )
 
     .map(
       (path) => `<url>
     ${
       path === "/index"
         ? `<loc>https://climatemonitor.info</loc>`
-        : `<loc>https://climatemonitor.info${path}</loc>`
+        : path === "/data/index"
+        ? `<loc>https://climatemonitor.info/data/</loc>`
+        : `<loc>https://climatemonitor.info${path}/</loc>`
     }
     <lastmod>${
       pathsObj[path].lastModified
@@ -32,7 +49,12 @@ const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
     }</lastmod>
   </url>`
     )}
+  ${datasets.map(
+    (path) => `<url><loc>https://climatemonitor.info/data/${path}/</loc>
+    <lastmod>${today}</lastmod>
+  </url>`
+  )}
 </urlset>`
 
-fs.writeFileSync("public/sitemap_local.xml", sitemapXml)
+fs.writeFileSync("out/sitemap_local.xml", sitemapXml)
 console.log("sitemap_local.xml saved!")
